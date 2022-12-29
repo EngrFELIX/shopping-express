9467c0a582fe219da5d2e1e7c202493920ae63de
Felix Oyanoghafo <lyxrox@gmail.com>
2022-12-29 13:03:52 +0100

    my first commit to shopping express

---
+++ data-storagre/index.js
@@ -0,0 +1,9 @@
+const items_array = [
+    { "name": "bread", "id": 1, qty: 2, price: 800 },
+    { "name": "sardines", "id": 2, qty: 3 ,price: 600},
+    { "name": "blue band", "id": 3, qty: 1 ,price: 1000},
+    { "name": "nestle milo", "id": 4, qty: 1 ,price: 6500},
+    { "name": "nutella", "id": 5, qty: 4  ,price: 4000}
+];
+
+module.exports = items_array
